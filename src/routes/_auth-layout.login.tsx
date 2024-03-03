d3a56fb570d2400a2e820532a459fb1afec5f00d
frustrated018 <niloyd018@gmail.com>
2024-03-03 16:42:09 +0600

    Basic layout for the login page is done

--- src/routes/_auth-layout.login.tsx
+++ src/routes/_auth-layout.login.tsx
@@ -1,3 +1,7 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Link, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth-layout/login")({
@@ -6,9 +10,43 @@ export const Route = createFileRoute("/_auth-layout/login")({
 
 function LoginComponent() {
   return (
-    <div>
-      <p>Login form will be here</p>
-      <Link to="/signup">Signup</Link>
+    <div className="flex justify-center items-center h-full">
+      <Card className="flex flex-col gap-5 h-96 w-full md:w-[70%] lg:w-[90%] xl:w-[70%] mx-auto">
+        <h3 className="text-center font-semibold text-4xl my-5">Login!</h3>
+        <CardContent>
+          <form className="space-y-5">
+            <div>
+              <Label htmlFor="email">Email:</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password:</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="password"
+              />
+            </div>
+            <Button className="w-full text-lg font-medium" type="submit">
+              Login
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <div className="inline-flex items-center">
+            Don&apos;t have an accout?
+            <Link to="/signup" className={buttonVariants({ variant: "link" })}>
+              Signup
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
